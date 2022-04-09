b8d0a61b6ca2f397061880467fa122f0a1abdd08
kroneldo28 <kroneldo28@yahoo.fr>
2022-04-09 05:25:22 +0100

    Started calling the Sunrise-Sunset API

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 
+
+CITY = "Douala"
+MY_LAT = 4.051056
+MY_LNG = 9.767869
+
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response)
 
@@ -9,3 +14,10 @@ latitude = data["iss_position"]["latitude"]
 iss_position = (longitude, latitude)
 print(iss_position)
 
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG
+}
+response_sunrise = requests.get(url=" https://api.sunrise-sunset.org/json", params=parameters)
+print(response_sunrise.json())
